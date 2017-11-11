2d196820d6893e24a97cf9163c09899b42a53a78
Lauri1010 <Laupatrik@gmail.com>
2017-11-11 16:10:29 +0200

    readme update
    Readme update

--- unpack.bat
+++ unpack.bat
@@ -1,4 +1,5 @@
 @echo off
-robocopy C:\githubProjects\tFramework C:\xampp\framework /zb /e /s /copyall /XD C:\githubProjects\tFramework\htdocs /XD C:\githubProjects\tFramework\nodejs
+robocopy C:\githubProjects\tFramework C:\newxampp\framework /zb /e /s /copyall /XD C:\githubProjects\tFramework\htdocs /XD C:\githubProjects\tFramework\nodejs /XD C:\githubProjects\tFramework\apps
+robocopy C:\githubProjects\tFramework\apps C:\newxampp\framework /zb /e /s /copyall /XD C:\githubProjects\tFramework\apps
 robocopy C:\githubProjects\tFramework\nodejs\app C:\nodejs\app /zb /e /s /copyall 
 pause
